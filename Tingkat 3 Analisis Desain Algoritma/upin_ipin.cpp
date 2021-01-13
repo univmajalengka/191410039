1d13668614e22129aa9f74b466db1be9e3b96239
iidmuiz04 <iidmuiz04@gmail.com>
2021-01-13 11:11:58 +0700

    Looping Upin Ipin

--- Tingkat 3 Analisis Desain Algoritma/upin_ipin.cpp
+++ Tingkat 3 Analisis Desain Algoritma/upin_ipin.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int n;
 
-main(){
+int main(){
 	cout<<"Masukan Nilai (1-1000) = ";
 	cin>>n;
 	cout<<endl;
 	
-	for(int x=1; x<n; x++){
-		if((x%3!=0) && (x%5!=0))
-		{
-	  cout<<x<<endl;
-	}
-		if(x%3==0){
-			cout<<"UPIN "<<endl;
-	}
-		if(x%5==0){
-			cout<<"IPIN "<<endl;
-	}
+	for(int x=1; x<=n; x++){
+		if((x%3==0) && (x%5==0)){
+		cout<<"UPIN IPIN"<<endl;
+	  	cout<<x<<endl;
+	}else if(x%3==0){
+		cout<<"UPIN "<<endl;
+	}else if(x%5==0){
+		cout<<"IPIN "<<endl;
+	}else{
+		cout<<x<<endl;
+		}
 	}
 	
 	cout<<endl;
